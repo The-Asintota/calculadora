6036db92a91db4438cab5a6cb436133666075f41
The-Asintota <aguizandre.2803@gmail.com>
2023-12-10 17:24:54 -0500

    feat(recentOperations-ui): add initial "recent operations" section and global styles
    - Implement the initial user interface for the "recent operations" section, which displays a list of the most recent calculations performed on the calculator.
    - Define global styles to ensure a consistent look and feel across the main interface of the web-based calculator.

---
+++ src/components/recentOperations.jsx
@@ -0,0 +1,28 @@
+import "../../styles/recentOperations.css"
+
+
+export const RecentOperations = () => {
+    return (
+        <section className="ro-recentOperations">
+            <h2 className="ro-title">Historial</h2>
+            <article className="ro-content">
+                <header>
+                    <h3>Operaciones recientes:</h3>
+                </header>
+
+                <ul className="ro-content-items">
+                    <li className="item">
+                        <div className="item-operation">1+1</div>
+                        <div className="item-result">= 2</div>
+                    </li>
+                </ul>
+            </article>
+
+            <div class="ro-clean">
+                <button class="ro-clean-button">
+                    Limpiar Historial
+                </button>
+            </div>
+        </section>
+    )
+}
