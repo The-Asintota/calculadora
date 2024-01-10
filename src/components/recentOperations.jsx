7b7d4bd57bf5b4bf6cbdecafa3697d97f9ff5b83
The-Asintota <aguizandre.2803@gmail.com>
2024-01-10 18:06:43 -0500

    feat(recentOperations): update logic and UI
    The state management of some props in the recentOperations component has been updated. Additionally, several modifications have been made to the UI to improve user experience.

--- src/components/recentOperations.jsx
+++ src/components/recentOperations.jsx
@@ -1,7 +1,33 @@
+import { useState, useEffect } from "react"
+import { v4 as uuidv4 } from 'uuid'
+import { TransitionGroup, CSSTransition } from 'react-transition-group'
 import "../styles/recentOperations.css"
 
 
-export const RecentOperations = () => {
+const FADE_OUT_DURATION = 500
+
+
+export const RecentOperations = ({ result, operation }) => {
+    const [operationsState, setOperations] = useState([])
+    const [ulClass, setUlClass] = useState('')
+
+    const handleButton = () => {
+        setUlClass(' fade-out')
+        setOperations([])
+        setTimeout(() => {
+            setUlClass('')
+        }, FADE_OUT_DURATION)
+    }
+
+    useEffect(() => {
+        if (operation) {
+            setOperations(prevOperations => [
+                { id: uuidv4(), op: operation, re: result },
+                ...prevOperations
+            ])
+        }
+    }, [operation])
+
     return (
         <section className="recentOperation">
             <h2 className="recentOperation__title">Historial</h2>
@@ -10,16 +36,20 @@ export const RecentOperations = () => {
                     <h3>Operaciones recientes:</h3>
                 </header>
 
-                <ul className="content__items">
-                    <li className="item">
-                        <div className="itemOperation">1+1</div>
-                        <div className="itemResult">= 2</div>
-                    </li>
-                </ul>
+                <TransitionGroup component="ul" className={`content__items${ulClass}`}>
+                    {operationsState.map((data) => (
+                        <CSSTransition key={data.id} timeout={FADE_OUT_DURATION} classNames="item">
+                            <li className="item">
+                                <div className="itemOperation">{data.op}</div>
+                                <div className="itemResult">{`= ${data.re}`}</div>
+                            </li>
+                        </CSSTransition>
+                    ))}
+                </TransitionGroup>
             </article>
 
             <div className="recentOperation__clean">
-                <button className="cleanButton">
+                <button className="cleanButton" onClick={handleButton}>
                     Limpiar Historial
                 </button>
             </div>
