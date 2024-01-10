1b7c404a960e3b9e1788607ef1f8244aff3cf914
The-Asintota <aguizandre.2803@gmail.com>
2024-01-09 19:10:59 -0500

    refactor: update main components and global styles
    The code of the main components and the global styles of the application were refactored to improve readability and maintainability. This includes changes in the app.jsx, main.jsx, and the styles in the styles/ directory.

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { WebCalculator } from './app.jsx'
+import { App } from './app.jsx'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-    <WebCalculator />
+    <App />
 )
